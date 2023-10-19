c673a94545afba858388cddcf29612686c9d0343
Jo <johj703@nate.com>
2023-10-19 22:11:26 +0900

    Next.js 13버전 기준으로 tailwindcss.config.js 경로 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{tsx,ts,js,jsx}",
+    "./src/components/**/*.{tsx,ts,js,jsx}",
   ],
   theme: {
     extend: {},
