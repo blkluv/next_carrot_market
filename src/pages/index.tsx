75e2916780655be3489577e57aec2008a3d985a2
Jo <johj703@nate.com>
2023-10-20 18:54:03 +0900

    컴포넌트 사이를 gap-10으로 늘림

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="grid gap-5 px-5 py-10 bg-slate-400">
+    <div className="grid gap-10 px-5 py-10 bg-slate-400">
       <div className="p-10 bg-white rounded-2xl"></div>
       <div className="p-10 bg-white rounded-2xl"></div>
       <div className="p-10 bg-white rounded-2xl"></div>
