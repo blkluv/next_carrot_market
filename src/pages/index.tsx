11e2f21716c916ab0cc14f9c5ff0a2bfee6551cb
Jo <johj703@nate.com>
2023-10-20 18:31:34 +0900

    div 박스 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className="flex flex-col px-5 py-10 space-y-5 bg-slate-400">
+      <div className="p-10 bg-white rounded-2xl"></div>
+      <div className="p-10 bg-white rounded-2xl"></div>
+      <div className="p-10 bg-white rounded-2xl"></div>
+      <div className="p-10 bg-white rounded-2xl"></div>
+    </div>
+  );
 };
 
 export default Home;
