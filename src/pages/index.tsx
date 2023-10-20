2f4232caedf2df6185961f756296088146d0348d
Jo <johj703@nate.com>
2023-10-20 18:52:23 +0900

    flex와 flex-col 대신에 grid와 gap을 사용해서 컴포넌트를 나눌 수도 있다.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col px-5 py-10 space-y-5 bg-slate-400">
+    <div className="grid gap-5 px-5 py-10 bg-slate-400">
       <div className="p-10 bg-white rounded-2xl"></div>
       <div className="p-10 bg-white rounded-2xl"></div>
       <div className="p-10 bg-white rounded-2xl"></div>
