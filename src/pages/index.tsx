110d68444ce694a06504e2cbd0f6b66d5533016c
Jo <johj703@nate.com>
2023-10-22 03:24:23 +0900

    색상 선택 할 때, 선택한 색상 주위로 집중할 수 있는 효과 작성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,9 +57,9 @@ const Home: NextPage = () => {
           <span className="text-xs text-gray-500">Chair</span>
           <div className="flex items-center justify-between mt-3 mb-5">
             <div className="space-x-2">
-              <button className="w-5 h-5 bg-yellow-500 rounded-full" />
-              <button className="w-5 h-5 bg-indigo-500 rounded-full" />
-              <button className="w-5 h-5 bg-teal-500 rounded-full" />
+              <button className="w-5 h-5 transition bg-yellow-500 bg-opacity-50 rounded-full focus:ring-2 ring-offset-2 ring-yellow-500" />
+              <button className="w-5 h-5 transition bg-indigo-500 rounded-full focus:ring-2 ring-offset-2 ring-indigo-500" />
+              <button className="w-5 h-5 transition bg-teal-500 rounded-full focus:ring-2 ring-offset-2 ring-teal-500" />
             </div>
             <div>
               <input type="radio" />
