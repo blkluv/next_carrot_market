8b72e9ca2053fb1afd3dbac7e77fab1a9f8eb0b8
Jo <johj703@nate.com>
2023-10-21 17:04:02 +0900

    그림자 효과 주기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,11 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="grid gap-10 px-5 py-10 bg-slate-400">
-      <div className="p-10 bg-white rounded-2xl"></div>
-      <div className="p-10 bg-white rounded-2xl"></div>
-      <div className="p-10 bg-white rounded-2xl"></div>
-      <div className="p-10 bg-white rounded-2xl"></div>
+    <div className="grid gap-10 px-10 py-20 bg-slate-400">
+      <div className="p-10 bg-white shadow-xl rounded-2xl"></div>
+      <div className="p-10 bg-white shadow-xl rounded-2xl"></div>
+      <div className="p-10 bg-white shadow-xl rounded-2xl"></div>
+      <div className="p-10 bg-white shadow-xl rounded-2xl"></div>
     </div>
   );
 };
