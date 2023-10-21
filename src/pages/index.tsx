84625e4c44845cbbee402105102e94294d20a927
Jo <johj703@nate.com>
2023-10-21 19:28:53 +0900

    Profile 카드 만들기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="grid gap-10 px-10 py-20 bg-slate-400">
+    <div className="grid gap-10 px-20 py-20 bg-slate-400">
       <div className="p-6 bg-white shadow-xl rounded-3xl">
         <span className="text-3xl font-semibold">Select Item</span>
         <div className="flex justify-between my-2">
@@ -21,7 +21,28 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className="p-10 bg-white shadow-xl rounded-2xl"></div>
+      <div className="overflow-hidden bg-white shadow-xl rounded-3xl">
+        <div className="p-6 bg-blue-500 pb-14">
+          <span className="text-2xl text-white">Profile</span>
+        </div>
+        <div className="relative p-6 bg-white rounded-3xl -top-5">
+          <div className="relative flex items-end justify-between -top-16">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="w-24 h-24 bg-red-400 rounded-full" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
+          </div>
+          <div className="relative flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-lg font-medium">Tony Molloy</span>
+            <span className="text-sm text-gray-500">미국</span>
+          </div>
+        </div>
+      </div>
       <div className="p-10 bg-white shadow-xl rounded-2xl"></div>
       <div className="p-10 bg-white shadow-xl rounded-2xl"></div>
     </div>
