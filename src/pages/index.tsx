698a6e98a6bb9a4d7a8548162782b00595c38fcc
Jo <johj703@nate.com>
2023-10-21 18:13:15 +0900

    컴포넌트 마크업 코드 만들기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,22 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="grid gap-10 px-10 py-20 bg-slate-400">
-      <div className="p-10 bg-white shadow-xl rounded-2xl"></div>
+      <div className="p-10 bg-white shadow-xl rounded-2xl">
+        <span>Select Item</span>
+        <div>
+          <span>Gray Chair</span>
+          <span>$19</span>
+        </div>
+        <div>
+          <span>Gray Chair</span>
+          <span>$19</span>
+        </div>
+        <div>
+          <span>Total</span>
+          <span>$10</span>
+        </div>
+        <div>Checkout</div>
+      </div>
       <div className="p-10 bg-white shadow-xl rounded-2xl"></div>
       <div className="p-10 bg-white shadow-xl rounded-2xl"></div>
       <div className="p-10 bg-white shadow-xl rounded-2xl"></div>
