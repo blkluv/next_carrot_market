2bc63f10f94bde7bf9cb714a3e072dc4df4d3869
Jo <johj703@nate.com>
2023-10-23 15:37:51 +0900

    Update index2.tsx

--- src/pages/index2.tsx
+++ src/pages/index2.tsx
@@ -5,6 +5,7 @@ const Home: NextPage = () => {
     <div className="flex flex-col p-5 space-y-2">
       <details>
         <summary>What is my fav. food.</summary>
+        <span>김치</span>
       </details>
     </div>
   );
