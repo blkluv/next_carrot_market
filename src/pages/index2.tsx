78ad3de347f2f80f1a4a4cbfb6cb6ac81a4b2600
Jo <johj703@nate.com>
2023-10-23 15:48:06 +0900

    drag 했을 때, 글자가 흰 색으로 보이도록 작성

--- src/pages/index2.tsx
+++ src/pages/index2.tsx
@@ -5,7 +5,9 @@ const Home: NextPage = () => {
     <div className="flex flex-col p-5 space-y-2">
       <details>
         <summary className="cursor-pointer">What is my fav. food.</summary>
-        <span className="selection:bg-indigo-600">김치</span>
+        <span className="selection:bg-indigo-600 selection:text-white">
+          김치
+        </span>
       </details>
     </div>
   );
