bbcad9df0b60d52af05f431ace5ff7d46d6a3da9
Jo <johj703@nate.com>
2023-10-23 15:10:18 +0900

    Create index2.tsx

---
+++ src/pages/index2.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex flex-col p-5 space-y-2">
+      <details>
+        <summary>What is my fav. food.</summary>
+      </details>
+    </div>
+  );
+};
+
+export default Home;
