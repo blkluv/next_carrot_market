7cb6bab6f0eec2d6aa32806be3d2e494b61879d9
Jo <johj703@nate.com>
2023-10-23 16:17:00 +0900

    리스트 목록을 점으로 표시

--- src/pages/index2.tsx
+++ src/pages/index2.tsx
@@ -3,10 +3,11 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col p-5 space-y-2">
-      <details className="select-none open:text-white open:bg-indigo-400">
-        <summary className="cursor-pointer">What is my fav. food.</summary>
-        <span>김치</span>
-      </details>
+      <ul className="list-disc marker:text-teal-500">
+        <li>hi</li>
+        <li>hi</li>
+        <li>hi</li>
+      </ul>
     </div>
   );
 };
