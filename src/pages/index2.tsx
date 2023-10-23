bca9d7418e79308ebcd1ddd8c5ad00af4100d606
Jo <johj703@nate.com>
2023-10-23 17:08:34 +0900

    file selector 꾸미기

--- src/pages/index2.tsx
+++ src/pages/index2.tsx
@@ -5,7 +5,7 @@ const Home: NextPage = () => {
     <div className="flex flex-col p-5 space-y-2">
       <input
         type="file"
-        className="file:cursor-pointer file:transition-colors file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400"
+        className="file:cursor-pointer file:hover:text-purple-400 file:hover:bg-white file:hover:border-purple-400 file: file:transition-colors file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400"
       />
     </div>
   );
