c43fd9a07e0241150c0169508581d02a9ccd80db
Jo <johj703@nate.com>
2023-10-23 16:48:45 +0900

    Update index2.tsx

--- src/pages/index2.tsx
+++ src/pages/index2.tsx
@@ -3,7 +3,10 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col p-5 space-y-2">
-      <input type="file" className="file:border-0 file:rounded-md" />
+      <input
+        type="file"
+        className="file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400"
+      />
     </div>
   );
 };
