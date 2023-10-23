8b4de7680db459b8019ca49b5868a0ec54f7f699
Jo <johj703@nate.com>
2023-10-23 16:25:27 +0900

    파일 선택 버튼 모서리를 둥글게 작성

--- src/pages/index2.tsx
+++ src/pages/index2.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col p-5 space-y-2">
-      <input type="file" className="file:border-0" />
+      <input type="file" className="file:border-0 file:rounded-md" />
     </div>
   );
 };
