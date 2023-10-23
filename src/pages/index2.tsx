fd18b2320b513371370b45e05fcee81680551894
Jo <johj703@nate.com>
2023-10-23 17:12:51 +0900

    텍스트에 커서가 도달하면 색이 보라색으로 변경하도록 작성

--- src/pages/index2.tsx
+++ src/pages/index2.tsx
@@ -3,7 +3,9 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col p-5 space-y-2">
-      <p className="first-letter:text-7xl">Hello everyone!</p>
+      <p className="first-letter:text-7xl first-letter:hover:text-purple-400">
+        Hello everyone!
+      </p>
     </div>
   );
 };
