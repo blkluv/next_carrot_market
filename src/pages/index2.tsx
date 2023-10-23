1fff0b1a717967b1b871544330ac77b0b50c7e01
Jo <johj703@nate.com>
2023-10-23 17:11:32 +0900

    첫 글자만 크도록 작성

--- src/pages/index2.tsx
+++ src/pages/index2.tsx
@@ -3,10 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col p-5 space-y-2">
-      <input
-        type="file"
-        className="file:cursor-pointer file:hover:text-purple-400 file:hover:bg-white file:hover:border-purple-400 file: file:transition-colors file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400"
-      />
+      <p className="first-letter:text-7xl">Hello everyone!</p>
     </div>
   );
 };
