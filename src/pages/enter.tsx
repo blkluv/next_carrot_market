9fcaaae0bbadb6be2dcf86e0743a67298e95a8aa
HyunJun-Jo <johj703@nate.com>
2023-11-06 23:03:56 +0900

    Update enter.tsx

--- src/pages/enter.tsx
+++ src/pages/enter.tsx
@@ -37,16 +37,22 @@ export default function Enter() {
             </button>
           </div>
         </div>
-        <form>
-          <label>
+        <form className="flex flex-col mt-8">
+          <label className="text-sm font-medium text-gray-700">
             {method === "email" ? "Email address" : null}
             {method === "phone" ? "Phone number" : null}
           </label>
-          <div>
-            {method === "email" ? <input type="email" required /> : null}
+          <div className="mt-1">
+          {method === "email" ? (
+              <input
+                type="email"
+                className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+                required
+              />
+            ) : null}
             {method === "phone" ? (
               <div>
-                <span>+82</span>
+                <span className="flex items-center justify-center px-3 text-sm text-gray-500 border border-r-0 border-gray-300 select-none rounded-l-md bg-gray-50">+82</span>
                 <input type="number" required />
               </div>
             ) : null}
