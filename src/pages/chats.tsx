42f23f8f5fe73bb1ad941b146f06d7597be32b63
Jo <johj703@nate.com>
2023-11-24 15:19:16 +0900

    border-t 삭제, 폰트 크기 조절, 메세지 수정

--- src/pages/chats.tsx
+++ src/pages/chats.tsx
@@ -2,12 +2,12 @@ import type { NextPage } from "next";
 
 const Chats: NextPage = () => {
     return <div className="px-4 py-10">
-        <div className="flex items-center py-3 space-x-3 border-t border-b cursor-pointer">
+        <div className="flex items-center py-3 space-x-3 border-b cursor-pointer">
           <div className="w-12 h-12 rounded-full bg-slate-300" />
           <div>
-            <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
-            <p className="text-xs font-medium text-gray-500">
-              View profile &rarr;
+            <p className="text-gray-700">Steve Jebs</p>
+            <p className="text-sm text-gray-500">
+                See you tomorrow in the corner at 2pm!
             </p>
           </div>
         </div>
