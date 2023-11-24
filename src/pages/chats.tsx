06684f4b77dd281a21d0101d7f487f38d3e3ed27
Jo <johj703@nate.com>
2023-11-24 15:32:33 +0900

    map을 사용해서 7개의 chat들을 만들고 마지막 chat의 아래쪽에 선이 나타나도록 작성

--- src/pages/chats.tsx
+++ src/pages/chats.tsx
@@ -1,17 +1,21 @@
 import type { NextPage } from "next";
 
 const Chats: NextPage = () => {
-    return <div className="py-10">
-        <div className="flex items-center px-4 py-3 space-x-3 border-b cursor-pointer">
-          <div className="w-12 h-12 rounded-full bg-slate-300" />
-          <div>
-            <p className="text-gray-700">Steve Jebs</p>
-            <p className="text-sm text-gray-500">
-                See you tomorrow in the corner at 2pm!
-            </p>
+    return (
+    <div className="py-10">
+        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+            <div key={i} className="flex items-center px-4 py-3 space-x-3 border-b cursor-pointer last:border-b-0">
+            <div className="w-12 h-12 rounded-full bg-slate-300" />
+            <div>
+              <p className="text-gray-700">Steve Jebs</p>
+              <p className="text-sm text-gray-500">
+                  See you tomorrow in the corner at 2pm!
+              </p>
+            </div>
           </div>
-        </div>
-    </div>;
+        ))}
+    </div>
+    )
 };
 
 export default Chats;
