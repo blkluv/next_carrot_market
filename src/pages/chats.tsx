22f8b706f324bbff94e717d74e53f68572df84e5
Jo <johj703@nate.com>
2023-11-24 15:21:11 +0900

    px-4를 자식 컴포넌트로 옮겨서 border-bottom을 좌우로 끝까지 늘림

--- src/pages/chats.tsx
+++ src/pages/chats.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
 
 const Chats: NextPage = () => {
-    return <div className="px-4 py-10">
-        <div className="flex items-center py-3 space-x-3 border-b cursor-pointer">
+    return <div className="py-10">
+        <div className="flex items-center px-4 py-3 space-x-3 border-b cursor-pointer">
           <div className="w-12 h-12 rounded-full bg-slate-300" />
           <div>
             <p className="text-gray-700">Steve Jebs</p>
