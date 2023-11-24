a9bbfce7b5a23ea4865f70d5a97d5624daf9ac33
Jo <johj703@nate.com>
2023-11-24 15:43:42 +0900

    필요없는 border-b를 삭제

--- src/pages/chats.tsx
+++ src/pages/chats.tsx
@@ -4,7 +4,7 @@ const Chats: NextPage = () => {
     return (
     <div className="py-10 divide-y-[1px]">
         {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-            <div key={i} className="flex items-center px-4 py-3 space-x-3 border-b cursor-pointer last:border-b-0">
+            <div key={i} className="flex items-center px-4 py-3 space-x-3 cursor-pointer last:border-b-0">
             <div className="w-12 h-12 rounded-full bg-slate-300" />
             <div>
               <p className="text-gray-700">Steve Jebs</p>
