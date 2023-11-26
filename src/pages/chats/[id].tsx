6b79f57b46448c440aa0cae5b9d80df6e332156d
Jo <johj703@nate.com>
2023-11-26 22:08:14 +0900

    대화 part css 작성2

--- src/pages/chats/[id].tsx
+++ src/pages/chats/[id].tsx
@@ -5,13 +5,15 @@ const ChatDetail: NextPage = () => {
     <div className="px-4 py-10">
       <div className="flex items-start space-x-2">
         <div className="w-8 h-8 rounded-full bg-slate-400" />
-        <div className="w-1/2 p-2 text-sm text-gray-700 border rounded-md">
+        <div className="w-1/2 p-2 text-sm text-gray-700 border border-gray-300 rounded-md">
           <p>Hi how much are you selling them for?</p>
         </div>
       </div>
-      <div>
-        <div />
-        <div>I want ￦20,000</div>
+      <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+        <div className="w-8 h-8 rounded-full bg-slate-400" />
+        <div className="w-1/2 p-2 text-sm text-gray-700 border border-gray-300 rounded-md">
+          <p>I want ￦20,000</p>
+        </div>
       </div>
       <div>
         <div />
