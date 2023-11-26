30803eec130fbaaa1aa0761f757fbca98f62b095
Jo <johj703@nate.com>
2023-11-26 22:30:32 +0900

    채팅 입력 part css 작성

--- src/pages/chats/[id].tsx
+++ src/pages/chats/[id].tsx
@@ -21,9 +21,12 @@ const ChatDetail: NextPage = () => {
           <p>미쳤어</p>
         </div>
       </div>
-      <div>
-        <div>
-          <input type="text" />
+      <div className="fixed inset-x-0 bottom-0 w-full max-w-md mx-auto">
+        <div className="flex items-center">
+          <input
+            type="text"
+            className="w-full border-gray-300 rounded-full shadow-sm focus:ring-orange-500 focus:outline-none focus:border-orange-500"
+          />
           <div>
             <span>&rarr;</span>
           </div>
