2675deb9106af0194deb6f3c23e69d257a0a3d58
Jo <johj703@nate.com>
2023-11-26 22:03:58 +0900

    대화 part css 작성

--- src/pages/chats/[id].tsx
+++ src/pages/chats/[id].tsx
@@ -3,9 +3,11 @@ import type { NextPage } from "next";
 const ChatDetail: NextPage = () => {
   return (
     <div className="px-4 py-10">
-      <div className="flex items-center space-x-2">
+      <div className="flex items-start space-x-2">
         <div className="w-8 h-8 rounded-full bg-slate-400" />
-        <div>Hi how much are you selling them for?</div>
+        <div className="w-1/2 p-2 text-sm text-gray-700 border rounded-md">
+          <p>Hi how much are you selling them for?</p>
+        </div>
       </div>
       <div>
         <div />
