e5381ae76df65261cfaea8e7be3b4bb8bcee296c
Jo <johj703@nate.com>
2023-11-29 23:17:16 +0900

    input 태그 id 변경

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -4,11 +4,11 @@ const EditProfile: NextPage = () => {
   return (
     <div className="px-4 py-10">
       <div className="space-y-1">
-        <label htmlFor="input" className="text-sm font-medium text-gray-700">
+        <label htmlFor="email" className="text-sm font-medium text-gray-700">
           Email address
         </label>
         <input
-          id="input"
+          id="email"
           type="email"
           className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-500 focus:border-orange-500"
           required
