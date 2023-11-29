aa37e6f6a33834e4d6302cbcf31a267806d07c6c
Jo <johj703@nate.com>
2023-11-29 22:57:16 +0900

    enter 컴포넌트에 있던 email address 코드 재사용

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from "next";
 
 const EditProfile: NextPage = () => {
-  return <div className="px-4 py-10"></div>;
+  return (
+    <div className="px-4 py-10">
+      <label htmlFor="input" className="text-sm font-medium text-gray-700">
+        Email address
+      </label>
+      <div className="mt-1">
+        <input
+          id="input"
+          type="email"
+          className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+          required
+        />
+      </div>
+    </div>
+  );
 };
 
 export default EditProfile;
