ee968172b24808893021eee8cd53d5931abaa5e2
Jo <johj703@nate.com>
2023-11-29 23:29:31 +0900

    Phone number 입력칸 추가

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -14,6 +14,22 @@ const EditProfile: NextPage = () => {
           required
         />
       </div>
+      <div className="space-y-1">
+        <label htmlFor="phone" className="text-sm font-medium text-gray-700">
+          Phone number
+        </label>
+        <div className="flex rounded-md shadow-sm">
+          <span className="flex items-center justify-center px-3 text-sm text-gray-500 border border-r-0 border-gray-300 select-none rounded-l-md bg-gray-50">
+            +82
+          </span>
+          <input
+            id="input"
+            type="number"
+            className="w-full px-4 py-2 placeholder-gray-400 border border-gray-300 rounded-md rounded-l-none shadow-sm appearance-none focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            required
+          />
+        </div>
+      </div>
     </div>
   );
 };
