861603e4c2b933db646e5879b8bb11d787e94735
Jo <johj703@nate.com>
2023-11-30 18:33:31 +0900

    Change 버튼 수정

--- src/pages/profile/edit.tsx
+++ src/pages/profile/edit.tsx
@@ -5,9 +5,12 @@ const EditProfile: NextPage = () => {
     <div className="px-4 py-10 space-y-4">
       <div className="flex items-center space-x-3">
         <div className="rounded-full w-14 h-14 bg-slate-500" />
-        <label>
-          Change photo
-          <input type="file" className="hidden" />
+        <label
+          htmlFor="picture"
+          className="px-3 py-2 text-sm font-medium text-gray-700 border border-gray-300 rounded-md shadow-sm cursor-pointer focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+        >
+          Change
+          <input id="picture" type="file" className="hidden" />
         </label>
       </div>
       <div className="space-y-1">
