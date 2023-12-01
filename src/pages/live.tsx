3618a5c99cddea831360659d5137b2e952b127b6
Jo <johj703@nate.com>
2023-12-01 17:02:05 +0900

    부모 div에 있던 px-4 옵션을 자식 div에 옮기면 테두리가 양 옆으로 쭉 늘어남!

--- src/pages/live.tsx
+++ src/pages/live.tsx
@@ -2,9 +2,9 @@ import type { NextPage } from "next";
 
 const Live: NextPage = () => {
   return (
-    <div className="px-4 py-10 space-y-4">
+    <div className="py-10 space-y-4 divide-y-2">
       {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div key={i}>
+        <div className="px-4 pt-4" key={i}>
           <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
           <h3 className="mt-2 text-lg text-gray-700">Let's try potatos</h3>
         </div>
