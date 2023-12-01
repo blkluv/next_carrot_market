bd2dba62bbdbf50a33a5fbd518e860f2cf83470b
Jo <johj703@nate.com>
2023-12-01 17:04:12 +0900

    index 페이지에서 만든 Plus 버튼을 복사/붙여넣기

--- src/pages/live.tsx
+++ src/pages/live.tsx
@@ -9,6 +9,23 @@ const Live: NextPage = () => {
           <h3 className="mt-2 text-lg text-gray-700">Let's try potatos</h3>
         </div>
       ))}
+      <button className="fixed p-4 text-white transition-colors bg-orange-400 rounded-full shadow-xl cursor-pointer hover:bg-orange-500 bottom-24 right-5">
+        <svg
+          className="w-6 h-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+      </button>
     </div>
   );
 };
