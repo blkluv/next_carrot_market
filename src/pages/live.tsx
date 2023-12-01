d3c752abaa5ce693d31a285593a1dbd4d70dd8af
Jo <johj703@nate.com>
2023-12-01 16:40:41 +0900

    aspect-video 옵션을 사용해서 자동으로 비디오의 비율을 줄 수 있다!

---
+++ src/pages/live.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Live: NextPage = () => {
+  return (
+    <div>
+      <div>
+        <div className="w-full bg-slate-300 aspect-video" />
+      </div>
+    </div>
+  );
+};
+
+export default Live;
