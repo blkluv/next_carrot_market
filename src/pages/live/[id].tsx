c485905bdca30dc92f4fa89d3aa0425909e0adc4
Jo <johj703@nate.com>
2023-12-02 15:43:31 +0900

    폰트 수정

--- src/pages/live/[id].tsx
+++ src/pages/live/[id].tsx
@@ -4,7 +4,9 @@ const LiveDetail: NextPage = () => {
   return (
     <div className="px-4 py-10 space-y-4">
       <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
-      <h3 className="mt-2 text-lg text-gray-700">Let's try potatos</h3>
+      <h3 className="mt-2 text-2xl font-semibold text-gray-800">
+        Let's try potatos
+      </h3>
     </div>
   );
 };
