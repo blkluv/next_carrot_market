ca7c4fd5c6bfa154c78d06b232aacc8fde7f5f25
Jo <johj703@nate.com>
2023-12-02 15:45:39 +0900

    Update [id].tsx

--- src/pages/live/[id].tsx
+++ src/pages/live/[id].tsx
@@ -5,7 +5,7 @@ const LiveDetail: NextPage = () => {
     <div className="px-4 py-10 space-y-4">
       <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
       <h3 className="mt-2 text-2xl font-semibold text-gray-800">
-        Let's try potatos
+        Let&apos;s try potatos
       </h3>
     </div>
   );
