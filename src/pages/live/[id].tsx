4168ecc383dafd86b5d97a760abe5be7dd0d363a
Jo <johj703@nate.com>
2023-12-02 16:06:27 +0900

    채팅 입력 칸 붙여넣기

--- src/pages/live/[id].tsx
+++ src/pages/live/[id].tsx
@@ -7,7 +7,7 @@ const LiveDetail: NextPage = () => {
       <h3 className="mt-2 text-2xl font-semibold text-gray-800">
         Let&apos;s try potatos
       </h3>
-      <div className="px-4 pb-16 mt-10 h-[50vh] overflow-y-scroll space-y-4">
+      <div className="px-4 pb-16 py-10 h-[50vh] overflow-y-scroll space-y-4">
         <div className="flex items-start space-x-2">
           <div className="w-8 h-8 rounded-full bg-slate-400" />
           <div className="w-1/2 p-2 text-sm text-gray-700 border border-gray-300 rounded-md">
@@ -126,6 +126,19 @@ const LiveDetail: NextPage = () => {
           <div className="w-8 h-8 rounded-full bg-slate-400" />
           <div className="w-1/2 p-2 text-sm text-gray-700 border border-gray-300 rounded-md">
             <p>I want ￦20,000</p>
+          </div>
+        </div>
+      </div>
+      <div className="fixed inset-x-0 w-full max-w-md mx-auto bottom-2">
+        <div className="relative flex items-center">
+          <input
+            type="text"
+            className="w-full pr-12 border-gray-300 rounded-full shadow-sm focus:ring-orange-500 focus:outline-none focus:border-orange-500"
+          />
+          <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+            <button className="flex items-center px-3 text-sm text-white bg-orange-500 rounded-full focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 hover:bg-orange-600">
+              &rarr;
+            </button>
           </div>
         </div>
       </div>
