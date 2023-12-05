ce707815168215a07ee973cc7a123ed23f1d049f
Jo <johj703@nate.com>
2023-12-05 17:59:44 +0900

    max-w-xl을 적용해서 오른쪽으로 치우친 홈 버튼을 가운데로 옮기기

--- components/layout.tsx
+++ components/layout.tsx
@@ -15,12 +15,12 @@ export default function Layout({
 }: LayoutProps) {
   return (
     <div>
-      <div className="fixed top-0 flex items-center justify-center w-full py-3 text-lg font-medium text-gray-800 bg-white border-b">
+      <div className="fixed top-0 flex items-center justify-center w-full max-w-xl py-3 text-lg font-medium text-gray-800 bg-white border-b">
         {title ? <span>{title}</span> : null}
       </div>
       <div className={cls("pt-16", hasTabBar ? "pb-16" : "")}>{children}</div>
       {hasTabBar ? (
-        <nav className="fixed bottom-0 flex items-center justify-between pt-3 pb-10 text-gray-800 bg-white border-t"></nav>
+        <nav className="fixed bottom-0 flex items-center justify-between max-w-xl pt-3 pb-10 text-gray-800 bg-white border-t"></nav>
       ) : null}
     </div>
   );
