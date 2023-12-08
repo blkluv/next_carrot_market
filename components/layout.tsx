ce00e1e1bef6507d0ca31e256d6931ee4b49e226
Jo <johj703@nate.com>
2023-12-08 14:27:59 +0900

    탭 이름 변경

--- components/layout.tsx
+++ components/layout.tsx
@@ -45,19 +45,18 @@ export default function Layout({
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              strokeWidth="1.5"
+              strokeWidth={1.5}
               stroke="currentColor"
               className="w-6 h-6"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                strokeWidth="2"
-                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"
               />
             </svg>
 
-            <span>홈</span>
+            <span>동네생활</span>
           </div>
           <div className="flex flex-col items-center space-y-2">
             <svg
@@ -76,7 +75,7 @@ export default function Layout({
               />
             </svg>
 
-            <span>홈</span>
+            <span>채팅</span>
           </div>
           <div className="flex flex-col items-center space-y-2">
             <svg
@@ -95,7 +94,7 @@ export default function Layout({
               />
             </svg>
 
-            <span>홈</span>
+            <span>LiveStream</span>
           </div>
           <div className="flex flex-col items-center space-y-2">
             <svg
@@ -114,7 +113,7 @@ export default function Layout({
               />
             </svg>
 
-            <span>홈</span>
+            <span>Profile</span>
           </div>
         </nav>
       ) : null}
