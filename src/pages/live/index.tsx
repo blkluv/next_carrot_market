33a5fbb4921f8ea6a4619f9d1bd5fc26351fa4aa
Jo <johj703@nate.com>
2023-12-08 16:16:11 +0900

    제일 바깥쪽에 Layout 컴포넌트로 감싸기

--- src/pages/live/index.tsx
+++ src/pages/live/index.tsx
@@ -1,32 +1,36 @@
 import type { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const Live: NextPage = () => {
   return (
-    <div className="py-10 space-y-4 divide-y-2">
-      {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div className="px-4 pt-4" key={i}>
-          <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
-          <h3 className="mt-2 text-lg text-gray-700">
-            Let&apos;s try potatos.
-          </h3>
-        </div>
-      ))}
-      <button className="fixed p-4 text-white transition-colors bg-orange-400 border-transparent rounded-full shadow-xl cursor-pointer hover:bg-orange-500 bottom-24 right-5">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
-          />
-        </svg>
-      </button>
-    </div>
+    <Layout hasTabBar title="라이브">
+      <div className=" divide-y-[1px] space-y-4">
+        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <div className="px-4 pt-4" key={i}>
+            <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
+            <h1 className="mt-2 text-2xl font-bold text-gray-900">
+              Galaxy S50
+            </h1>
+          </div>
+        ))}
+        <button className="fixed p-4 text-white transition-colors bg-orange-400 border-transparent rounded-full shadow-xl cursor-pointer hover:bg-orange-500 bottom-24 right-5">
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </Layout>
   );
 };
 
