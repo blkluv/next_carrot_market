60f8b1b314680b8a6ae06bd40c8718fef013adb8
Jo <johj703@nate.com>
2023-12-12 17:43:00 +0900

    useState에 빈 배열 추가

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
 export default function Forms() {
-  const [username, setUsername] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <form>
       <input value={username} type="text" placeholder="Username" />
