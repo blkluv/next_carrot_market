fd92bfa3ab6a460516545dd58f1a7908ce985f4b
Jo <johj703@nate.com>
2023-12-12 18:11:41 +0900

    onusernameChange 함수 생성, username input 태그에 입력

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -4,9 +4,20 @@ export default function Forms() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const onUsernameChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setUsername(value);
+  };
   return (
     <form>
-      <input value={username} type="text" placeholder="Username" />
+      <input
+        onChange={onUsernameChange}
+        value={username}
+        type="text"
+        placeholder="Username"
+      />
       <input value={email} type="email" placeholder="Email" />
       <input value={password} type="password" placeholder="Password" />
     </form>
