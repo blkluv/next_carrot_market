f0111de45e544b9e9e11dd5df63908cc409965b3
Jo <johj703@nate.com>
2023-12-12 17:40:31 +0900

    input 태그와 useState 생성

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 export default function Forms() {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   return (
     <form>
+      <input type="text" placeholder="Username" />
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
     </form>
   );
 }
