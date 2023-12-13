46677e717e015af7b36a948ca7975889768d8a01
Jo <johj703@nate.com>
2023-12-13 20:59:02 +0900

    watch 함수 사용하기

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -9,11 +9,12 @@ import { useForm } from "react-hook-form";
 
 export default function Forms() {
   // 1. 모든 것은 useForm 훅에서 나온다!
-  const { register } = useForm();
-  console.log(register("name"));
+  const { register, watch } = useForm();
+  console.log(watch());
   return (
     <form>
       {/* 2. input들을 모두 state에 '등록'하기 위해서는, register 함수를 사용한다 */}
+      {/* 2. watch함수를 사용하기 위해서는 input 태그에서 register 함수를 사용한다. */}
       <input
         {...register("username")}
         type="text"
