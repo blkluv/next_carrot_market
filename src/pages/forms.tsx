0df9c625f3fa5bebd0fbf4a5cc66478db97f1612
Jo <johj703@nate.com>
2023-12-13 19:50:49 +0900

    register 사용하기

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 // Less code
 // Better validation
@@ -8,9 +8,15 @@ import { useState } from "react";
 // Easier Inputs
 
 export default function Forms() {
+  const { register } = useForm();
   return (
     <form>
-      <input type="text" placeholder="Username" required />
+      <input
+        {...register("username")}
+        type="text"
+        placeholder="Username"
+        required
+      />
       <input type="email" placeholder="Email" required />
       <input type="password" placeholder="Password" required />
       <input type="submit" value="Create Account" />
