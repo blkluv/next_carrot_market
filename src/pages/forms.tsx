b804fb3d09974e4d84f3894cb67fecc65a7f71a5
Jo <johj703@nate.com>
2023-12-19 21:26:07 +0900

    Update forms.tsx

--- src/pages/forms.tsx
+++ src/pages/forms.tsx
@@ -52,7 +52,6 @@ export default function Forms() {
         })}
         type="email"
         placeholder="Email"
-        className={`${Boolean(errors.email?.message) ? "border-red-500" : ""}`}
       />
       {errors.email?.message}
       <input
