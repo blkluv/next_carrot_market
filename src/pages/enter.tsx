129f4b41ecdd078b3f4f685b42555f6e68c94d46
Jo <johj703@nate.com>
2023-12-21 16:42:55 +0900

    Update enter.tsx

--- src/pages/enter.tsx
+++ src/pages/enter.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from "next";
 import { useState } from "react";
-import { cls } from "../libs/utils";
 import { useForm } from "react-hook-form";
+import Button from "../components/button";
 import Input from "../components/input";
+import { cls } from "../libs/utils";
 
 interface EnterForm {
   email?: string;
@@ -10,10 +11,19 @@ interface EnterForm {
 }
 
 const Enter: NextPage = () => {
-  const { register } = useForm<EnterForm>();
+  const { register, handleSubmit, reset } = useForm<EnterForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
-  const onEmailClick = () => setMethod("email");
-  const onPhoneClick = () => setMethod("phone");
+  const onEmailClick = () => {
+    reset();
+    setMethod("email");
+  };
+  const onPhoneClick = () => {
+    reset();
+    setMethod("phone");
+  };
+  const onValid = (data: EnterForm) => {
+    console.log(data);
+  };
   return (
     <div className="px-4 mt-16">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
@@ -45,35 +55,35 @@ const Enter: NextPage = () => {
             </button>
           </div>
         </div>
-        <form className="flex flex-col mt-8 space-y-4">
+        <form
+          onSubmit={handleSubmit(onValid)}
+          className="flex flex-col mt-8 space-y-4"
+        >
           {method === "email" ? (
             <Input
-              register={register("email")}
+              register={register("email", {
+                required: true,
+              })}
               name="email"
-              id="input"
+              label="Email address"
               type="email"
-              className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-500 focus:border-orange-500"
               required
             />
           ) : null}
           {method === "phone" ? (
-            <div className="flex rounded-md shadow-sm">
-              <span className="flex items-center justify-center px-3 text-sm text-gray-500 border border-r-0 border-gray-300 select-none rounded-l-md bg-gray-50">
-                +82
-              </span>
-              <input
-                id="input"
-                type="number"
-                className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md rounded-l-none shadow-sm appearance-none focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                required
-              />
-            </div>
+            <Input
+              register={register("phone")}
+              name="phone"
+              label="Phone number"
+              type="number"
+              kind="phone"
+              required
+            />
           ) : null}
-
-          <button className="px-4 py-2 mt-5 text-sm font-medium text-white bg-orange-500 border border-transparent rounded-md shadow-sm hover:bg-orange-600 focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
-            {method === "email" ? "Get login link" : null}
-            {method === "phone" ? "Get one-time password" : null}
-          </button>
+          {method === "email" ? <Button text={"Get login link"} /> : null}
+          {method === "phone" ? (
+            <Button text={"Get one-time password"} />
+          ) : null}
         </form>
         <div className="mt-8">
           <div className="relative">
@@ -85,7 +95,7 @@ const Enter: NextPage = () => {
             </div>
           </div>
           <div className="grid grid-cols-2 gap-3 mt-2">
-            <button className="flex items-center justify-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 hover:text-sky-500">
+            <button className="flex items-center justify-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50">
               <svg
                 className="w-5 h-5"
                 aria-hidden="true"
@@ -95,7 +105,7 @@ const Enter: NextPage = () => {
                 <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
               </svg>
             </button>
-            <button className="flex items-center justify-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 hover:text-gray-900">
+            <button className="flex items-center justify-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50">
               <svg
                 className="w-5 h-5"
                 aria-hidden="true"
