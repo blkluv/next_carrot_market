74bfa16b32842bbabe876d9be6ad2bca20857a19
Jo <johj703@nate.com>
2023-12-22 21:15:12 +0900

    Update enter.tsx

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -4,14 +4,4 @@ import client from "../../../libs/client";
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-) {
-  await client.user.create({
-    data: {
-      email: "hi",
-      name: "hi",
-    },
-  });
-  res.json({
-    ok: true,
-  });
-}
+) {}
