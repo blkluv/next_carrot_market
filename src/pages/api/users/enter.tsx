cf10bd8890364f704f07e6c1c0a75c31683fc308
Jo <johj703@nate.com>
2023-12-22 21:28:51 +0900

    콘솔 Network-Headers 탭에서 정상적으로 통신하는 것을 확인할 수 있다.

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -4,4 +4,7 @@ import client from "../../../libs/client";
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-) {}
+) {
+  console.log(req.body);
+  res.status(200).end();
+}
