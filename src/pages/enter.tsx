ebf54812e7aef2dbeeab3fc7183a6f6cbc908e3f
Jo <johj703@nate.com>
2023-12-24 22:49:22 +0900

    headers를 설정하면 req.body.email을 적었어도 콘솔창에 메일을 띄울 수 있다.

--- src/pages/enter.tsx
+++ src/pages/enter.tsx
@@ -25,6 +25,9 @@ const Enter: NextPage = () => {
     fetch("/api/users/enter", {
       method: "POST",
       body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
   };
   return (
