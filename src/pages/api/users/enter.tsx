cc0d4a4f5da3c62ffc9a5a117e5757e01ad27966
Jo <johj703@nate.com>
2023-12-25 21:49:34 +0900

    POST 요청이 아니라 GET 요청이라면 에러 코드를 콜솔에 나타낼 수 있도록 작성

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -5,6 +5,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method !== "POST") {
+    res.status(401).end();
+  }
   console.log(req.body.email);
   res.status(200).end();
 }
