30d8d876dac33b71dcbb787e18aabf88b358d95c
Jo <johj703@nate.com>
2023-12-26 23:48:08 +0900

    return할 object 추가

--- src/libs/client/useMutation.tsx
+++ src/libs/client/useMutation.tsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 export default function useMutation(url: string) {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<undefined | any>(undefined);
+  const [error, setError] = useState<undefined | any>(undefined);
   function mutation(data: any) {
-    return [mutation];
+    return [mutation, { loading, data, error }];
   }
 }
