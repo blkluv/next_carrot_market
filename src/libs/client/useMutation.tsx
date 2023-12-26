849ac5c7af26a4602a7723e80ec2113dab9b7732
Jo <johj703@nate.com>
2023-12-26 23:42:14 +0900

    useMutation 작성

--- src/libs/client/useMutation.tsx
+++ src/libs/client/useMutation.tsx
@@ -1,3 +1,9 @@
-export default function useMutation() {
-  return;
+import { useState } from "react";
+
+export default function useMutation(url: string) {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<undefined | any>(undefined);
+  function mutation(data: any) {
+    return [mutation];
+  }
 }
