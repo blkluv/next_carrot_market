18bafd08a3926dcb3a38171260639527f8106bd7
Jo <johj703@nate.com>
2023-12-27 21:12:51 +0900

    useMutation 수정

--- src/libs/client/useMutation.tsx
+++ src/libs/client/useMutation.tsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 
-export default function useMutation(
-  url: string
-): [
-  (data: any) => void,
-  { loading: boolean; data: undefined | any; error: undefined | any }
-] {
+interface UseMutationState {
+  loading: boolean;
+  data?: object;
+  error?: object;
+}
+type UseMutationResult = [(data: any) => void, UseMutationState];
+
+export default function useMutation(url: string): UseMutationResult {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<undefined | any>(undefined);
   const [error, setError] = useState<undefined | any>(undefined);
-  function mutation(data?: any) {
-    return [mutation, { loading, data, error }];
-  }
+  function mutation(data?: any) {}
+  return [mutation, { loading, data, error }];
 }
