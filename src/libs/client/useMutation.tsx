bd9a600914546ea49c6ddfd8c5763601f5936ffc
Jo <johj703@nate.com>
2023-12-28 12:35:03 +0900

    response.json()을 return하면 다른 promise를 받게된다

--- src/libs/client/useMutation.tsx
+++ src/libs/client/useMutation.tsx
@@ -11,6 +11,15 @@ export default function useMutation(url: string): UseMutationResult {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<undefined | any>(undefined);
   const [error, setError] = useState<undefined | any>(undefined);
-  function mutation(data?: any) {}
+  function mutation(data?: any) {
+    setLoading(true);
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((response) => response.json());
+  }
   return [mutation, { loading, data, error }];
 }
