762b67e70e26667769c6c69c4645a700206c56af
Jo <johj703@nate.com>
2023-12-29 17:55:17 +0900

    지워진 if문을 export defalut withHandler("POST", handler);가 대신한다!

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -2,9 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../../libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    res.status(401).end();
-  }
   console.log(req.body);
   res.status(200).end();
 }
