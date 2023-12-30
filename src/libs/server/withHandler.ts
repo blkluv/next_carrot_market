59e2d94be0faa599a42d772aa4dcb88b864414c0
Jo <johj703@nate.com>
2023-12-30 16:28:38 +0900

    NextApiRequest, NextApiResponse import

--- src/libs/server/withHandler.ts
+++ src/libs/server/withHandler.ts
@@ -1,3 +1,5 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
 export default function withHandler(
   method: "GET" | "POST" | "DELETE",
   fn: (req: NextApiRequest, res: NextApiResponse) => void
