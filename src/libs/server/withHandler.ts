c7f6789693b4cf4af60e6b10803c1f2fd8e0c490
Jo <johj703@nate.com>
2023-12-30 17:22:48 +0900

    api/users/enter 경로로 들어가면 "hello":true가 제대로 전달된 것을 볼 수 있다.

--- src/libs/server/withHandler.ts
+++ src/libs/server/withHandler.ts
@@ -3,4 +3,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default function withHandler(
   method: "GET" | "POST" | "DELETE",
   fn: (req: NextApiRequest, res: NextApiResponse) => void
-) {}
+) {
+  return async function (req: NextApiRequest, res: NextApiResponse) {
+    res.json({ hello: true });
+  };
+}
