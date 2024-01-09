54a4c845d2a4326cbad46438ed549d2f26d4f1ff
Jo <johj703@nate.com>
2024-01-09 18:47:50 +0900

    Update enter.tsx

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -5,42 +5,52 @@ import withHandler from "src/libs/server/withHandler";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   let user;
-  if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (user) console.log("found it.");
-    if (!user) {
-      console.log("Did not find. Will create.");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          email,
-        },
-      });
-    }
-    console.log(user);
-  }
-  if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,
-      },
-    });
-    if (user) console.log("found it.");
-    if (!user) {
-      console.log("Did not find. Will create.");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          phone: +phone,
-        },
-      });
-    }
-    console.log(user);
-  }
+  user = await client.user.upsert({
+    where: {
+      phone: +phone,
+    },
+    create: {
+      name: "Anonymous",
+      phone: +phone,
+    },
+    update: {},
+  });
+  // if (email) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email,
+  //     },
+  //   });
+  //   if (user) console.log("found it.");
+  //   if (!user) {
+  //     console.log("Did not find. Will create.");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         email,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+  // if (phone) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //   });
+  //   if (user) console.log("found it.");
+  //   if (!user) {
+  //     console.log("Did not find. Will create.");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         phone: +phone,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
   return res.status(200).end();
 }
 
