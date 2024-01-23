fd81e35a10e5c65a446bb132c771e048ce5fbc7a
Jo <johj703@nate.com>
2024-01-23 20:42:59 +0900

    Update confirm.tsx

--- src/pages/api/users/confirm.tsx
+++ src/pages/api/users/confirm.tsx
@@ -3,6 +3,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "src/libs/server/withHandler";
 import client from "src/libs/server/client";
 
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
+
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
@@ -15,7 +23,7 @@ async function handler(
   });
   if (!exists) res.status(404).end();
   req.session.user = {
-    id: exists?.userId,
+    id: exists.userId,
   };
   await req.session.save();
   res.status(200).end();
