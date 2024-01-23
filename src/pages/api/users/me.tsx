ff019565c69128e71852b4bdc8bd6ee4da42ed93
Jo <johj703@nate.com>
2024-01-23 22:12:54 +0900

    유저 프로필을 조회할 수 있도록 작성

--- src/pages/api/users/me.tsx
+++ src/pages/api/users/me.tsx
@@ -16,7 +16,13 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   console.log(req.session.user);
-  res.status(200).end();
+  const profile = await client.user.findUnique({
+    where: { id: req.session.user?.id },
+  });
+  res.json({
+    ok: true,
+    profile,
+  });
 }
 
 export default withIronSessionApiRoute(withHandler("GET", handler), {
