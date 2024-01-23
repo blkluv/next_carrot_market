4bcce3fa1ef1bae4e513bdfae8b4cd3cff72fc65
Jo <johj703@nate.com>
2024-01-23 21:03:15 +0900

    Create me.tsx

---
+++ src/pages/api/users/me.tsx
@@ -0,0 +1,25 @@
+import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "src/libs/server/withHandler";
+import client from "src/libs/server/client";
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  console.log(req.session.user)
+  res.status(200).end();
+}
+
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password: "789454168587496541879496519846541981965746851894651",
+});
