ab61f47caa7cca1b3f5218572a9d468ecdf5653a
Jo <johj703@nate.com>
2024-01-24 16:56:48 +0900

    useRouter, useEffect 추가

--- src/pages/enter.tsx
+++ src/pages/enter.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Button from "src/components/button";
 import Input from "src/components/input";
 import { cls } from "src/libs/client/utils";
 import useMutation from "src/libs/client/useMutation";
+import { useRouter } from "next/router";
 
 interface EnterForm {
   email?: string;
@@ -44,6 +45,12 @@ const Enter: NextPage = () => {
     if (tokenLoading) return;
     confirmToken(validForm);
   };
+  const router = useRouter();
+  useEffect(() => {
+    if (tokenData?.ok) {
+      router.push("/");
+    }
+  }, [tokenData]);
   return (
     <div className="px-4 mt-16">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
