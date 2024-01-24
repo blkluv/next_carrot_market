a995c223752a535f524f2531b3d816e60461a60d
Jo <johj703@nate.com>
2024-01-24 16:51:49 +0900

    Update confirm.tsx

--- src/pages/api/users/confirm.tsx
+++ src/pages/api/users/confirm.tsx
@@ -18,7 +18,7 @@ async function handler(
     id: exists.userId,
   };
   await req.session.save();
-  res.status(200).end();
+  res.json({ ok: true });
 }
 
 export default withApiSession(withHandler("POST", handler));
