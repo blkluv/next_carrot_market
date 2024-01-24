095da9c36353100b82d924927a00fcf02eaefeb8
Jo <johj703@nate.com>
2024-01-24 16:42:26 +0900

    iron-session 이동

--- src/pages/api/users/me.tsx
+++ src/pages/api/users/me.tsx
@@ -3,14 +3,6 @@ import withHandler, { ResponseType } from "src/libs/server/withHandler";
 import client from "src/libs/server/client";
 import { withApiSession } from "src/libs/server/withSession";
 
-declare module "iron-session" {
-  interface IronSessionData {
-    user?: {
-      id: number;
-    };
-  }
-}
-
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
