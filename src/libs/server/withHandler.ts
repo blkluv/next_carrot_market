75a2869eb6ea0a7cb83bcc25b72b62a860838bb9
Jo <johj703@nate.com>
2024-01-25 18:15:43 +0900

    로그인 해주세요 메세지 추가

--- src/libs/server/withHandler.ts
+++ src/libs/server/withHandler.ts
@@ -18,7 +18,7 @@ export default function withHandler(
       return res.status(405).end();
     }
     if (isPrivate && !req.session.user) {
-      return res.status(401).json({ ok: false });
+      return res.status(401).json({ ok: false, error: "Plz log in." });
     }
     try {
       await fn(req, res);
