7f93f76ee75a9fb6df65b995d3dab1f723f6eb3e
Jo <johj703@nate.com>
2024-01-25 18:55:04 +0900

    fn을 handler로 변경 및 config 삭제

--- src/libs/server/withHandler.ts
+++ src/libs/server/withHandler.ts
@@ -7,23 +7,27 @@ export interface ResponseType {
 
 interface ConfigType {
   method: "GET" | "POST" | "DELETE";
-  fn: (req: NextApiRequest, res: NextApiResponse) => void;
+  handler: (req: NextApiRequest, res: NextApiResponse) => void;
   isPrivate: boolean;
 }
 
-export default function withHandler(config: ConfigType) {
+export default function withHandler({
+  method,
+  isPrivate,
+  handler,
+}: ConfigType) {
   return async function (
     req: NextApiRequest,
     res: NextApiResponse
   ): Promise<any> {
-    if (req.method !== config.method) {
+    if (req.method !== method) {
       return res.status(405).end();
     }
-    if (config.isPrivate && !req.session.user) {
+    if (isPrivate && !req.session.user) {
       return res.status(401).json({ ok: false, error: "Plz log in." });
     }
     try {
-      await config.fn(req, res);
+      await handler(req, res);
     } catch (error) {
       console.log(error);
       return res.status(500).json({ error });
