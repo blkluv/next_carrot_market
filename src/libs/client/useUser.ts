8cc8dbba3985d074c9cf88a2386849d69a771afd
Jo <johj703@nate.com>
2024-01-26 23:34:54 +0900

    useEffect 추가

--- src/libs/client/useUser.ts
+++ src/libs/client/useUser.ts
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function useUser(){
     const [user, setUser] = useState();
+    useEffect(() => {
+        fetch("/api/users/me")
+    }, [])
     return user;
 }0
