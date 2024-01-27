89207b646d5c2be9132f8142765f8ab238cbbf55
Jo <johj703@nate.com>
2024-01-27 16:01:27 +0900

    response 작성

--- src/libs/client/useUser.ts
+++ src/libs/client/useUser.ts
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 
-export default function useUser(){
-    const [user, setUser] = useState();
-    useEffect(() => {
-        fetch("/api/users/me")
-    }, [])
-    return user;
-}0
+export default function useUser() {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    fetch("/api/users/me")
+      .then((response) => response.json())
+      .then((json) => {});
+  }, []);
+  return user;
+}
+0;
