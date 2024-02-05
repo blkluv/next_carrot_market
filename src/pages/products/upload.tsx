ecb7f6253b8d335dd65cd53f94312332efdd9249
Jo <johj703@nate.com>
2024-02-05 15:09:19 +0900

    중복이 된 Name, Price 삭제

--- src/pages/products/upload.tsx
+++ src/pages/products/upload.tsx
@@ -34,44 +34,6 @@ const Upload: NextPage = () => {
           <input className="hidden" type="file" />
         </label>
       </div>
-      <div>
-        <label
-          className="block mb-1 text-sm font-medium text-gray-700"
-          htmlFor="name"
-        >
-          Name
-        </label>
-        <div className="relative flex items-center rounded-md shadow-sm">
-          <input
-            id="name"
-            type="email"
-            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-            required
-          />
-        </div>
-      </div>
-      <div>
-        <label
-          className="block mb-1 text-sm font-medium text-gray-700"
-          htmlFor="price"
-        >
-          Price
-        </label>
-        <div className="relative flex items-center rounded-md shadow-sm">
-          <div className="absolute left-0 flex items-center justify-center pl-3 pointer-events-none">
-            <span className="text-sm text-gray-500">$</span>
-          </div>
-          <input
-            id="price"
-            className="w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none pl-7 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-            type="text"
-            placeholder="0.00"
-          />
-          <div className="absolute right-0 flex items-center pr-3 pointer-events-none">
-            <span className="text-gray-500">USD</span>
-          </div>
-        </div>
-      </div>
       <Input
         register={register("name", { required: true })}
         required
