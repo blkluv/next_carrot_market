061387d3f5a94419920814cb8cdd20ce42eaafb5
Jo <johj703@nate.com>
2024-02-06 18:41:01 +0900

    Update index.ts

--- src/pages/api/products/index.ts
+++ src/pages/api/products/index.ts
@@ -8,6 +8,7 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   const { name, price, description } = req.body;
+  const { user } = req.session;
   const profile = await client.user.findUnique({
     where: { id: req.session.user?.id },
   });
