ddb6f16c32f4e897b595b3a129df9a87f2bb1113
Jo <johj703@nate.com>
2024-02-06 18:33:32 +0900

    Create index.ts

---
+++ src/pages/api/products/index.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "src/libs/server/withHandler";
+import client from "src/libs/server/client";
+import { withApiSession } from "src/libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const profile = await client.user.findUnique({
+    where: { id: req.session.user?.id },
+  });
+  res.json({
+    ok: true,
+    profile,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    method: "GET",
+    handler,
+  })
+);
