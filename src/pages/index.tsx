5a58fe719ade5e164254c048669d7380297c2ab6
Jo <johj703@nate.com>
2024-02-08 18:49:38 +0900

    가짜 데이터 대신 진짜 데이터 입력

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,19 +8,19 @@ import useSWR from "swr";
 
 const Home: NextPage = () => {
   const { user, isLoading } = useUser();
-  const {} = useSWR("/api/products");
+  const { data } = useSWR("/api/products");
   return (
     <Layout title="홈" hasTabBar>
       <Head>
         <title>Home</title>
       </Head>
       <div className="flex flex-col space-y-5 divide-y">
-        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+        {data?.products?.map((product) => (
           <Item
-            id={i}
-            key={i}
-            title="iPhone 14"
-            price={99}
+            id={product.id}
+            key={product.id}
+            title={product.name}
+            price={product.price}
             comments={1}
             hearts={1}
           />
