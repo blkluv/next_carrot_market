dcb67b935caa86df513b35ba39a5100e7ca0ca75
Jo <johj703@nate.com>
2024-02-19 17:50:15 +0900

    id와 user 추가

--- src/pages/api/products/[id]/index.ts
+++ src/pages/api/products/[id]/index.ts
@@ -7,7 +7,10 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { id } = req.query;
+  const {
+    query: { id },
+    session: { user },
+  } = req;
   const product = await client.product.findUnique({
     where: {
       id: Number(id),
